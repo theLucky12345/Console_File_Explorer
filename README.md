606f8ac1a9302f7c1a04d442436beb8ffa8ff131
theLucky12345 <122748704+theLucky12345@users.noreply.github.com>
2023-01-25 00:08:39 +0300

    v1.1-beta
    #added function size
    #creating README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# Console_File_Explorer #
+
+## I want to do: ##
+```
+search {file_name/directory_name} -> this method for finding files or directories
+```
+
+```
+open {file_name} -> this method for opeing files
+```
+
+```
+chdsk {disk_name} -> this method for changing disk for seening files or dictories
+```
+
+### This project is open source, but if you take something from here, then please leave a link to me! 😅😥😓 ###
+
+# 😆 GOOD LUCK 😆 #
--- program/Program.cpp
+++ program/Program.cpp
@@ -1,5 +1,19 @@
 #include "Program.h"
 
+// private methods
+struct Program::ReadableSize {
+    std::uintmax_t size{};
+private:
+    friend std::ostream &
+    operator<<(std::ostream &os, ReadableSize readableSize) {
+        int i{};
+        double mantissa = readableSize.size;
+        for (; mantissa >= 1024.; mantissa /= 1024., ++i) {}
+        printf("%4.2f %c", mantissa, "BKMGTPE"[i]);
+        return i == 0 ? os : os << "B (" << readableSize.size << ')';
+    }
+};
+
 // public methods
 void Program::setup() {
     std::cout << "<" << std::filesystem::current_path().string() << ">";
@@ -57,5 +71,35 @@ void Program::setup() {
         } catch (std::exception &exc) {
             std::cout << exc.what() << std::endl;
         }
+    } else if (in.starts_with("size")) {
+        try {
+            if (std::filesystem::directory_entry().is_directory()) {
+                //TODO
+            } else {
+                std::cout << in.substr(in.find(" ") + 1) << " -> "
+                          << ReadableSize{std::filesystem::file_size(in.substr(in.find(" ") + 1))} << std::endl;
+            }
+        } catch (std::exception &exc) {
+            std::cout << exc.what() << std::endl;
+        }
+    } else if (in.starts_with("clear") || in.starts_with("cls") && in.length() == 5 || in.length() == 2) {
+        system("cls");
+    } else if (in.starts_with("exit")) {
+        exit(0);
+    } else if (in.starts_with("help") && in.length() == 4) {
+        std::cout << "type 'help' for help" << std::endl
+             << "type 'clear' to clear screen" << std::endl
+             << "type 'exit' for exiting from program" << std::endl
+             << "type 'ls' to view files/folder in current directory"
+             << "type 'cd ../ cd' to go back" << std::endl
+             << "type 'cd {folder_name}' to open folder" << std::endl
+             << "type 'mkdir {folder_name}' to create folder" << std::endl
+             << "type 'touch {file_name}' to create file" << std::endl
+             << "type 'rm {file_name/folder_name}' to remove file/folder" << std::endl
+             << "type 'copy {file_name/folder_name}' to copy file/folder" << std::endl
+             << "type 'move {file_name/folder_name}' to move file/folder" << std::endl
+             << "type 'size {file_name/folder_name}' to view size of file/folder" << std::endl;
+    } else {
+        std::cout << "Invalid input.." << std::endl << "\tTry typing 'help' for helping yourself";
     }
 }
